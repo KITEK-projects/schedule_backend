2a75adef151d3596598a76ab0aa187c37a6de94c
XaoriGit <xaoriNit@gmail.com>
2024-11-04 20:57:58 +0600

    add error in ClientsApiView
    Добавил обработку ошибок на пустой список клиентов плюс изменил немного спецификацию

--- api/schedule_api/views.py
+++ api/schedule_api/views.py
@@ -77,11 +77,17 @@ class ClientsApiView(APIView):
             elif is_teacher.lower() == 'false':
                 is_teacher = False
             else:
-                return Response({'error': 'Параметр is_teacher должен быть true или false'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'error': 'Параметр is_teacher должен быть true или false'}, 
+                                status=status.HTTP_400_BAD_REQUEST)
 
-            clients_list = clients.objects.filter(is_teacher=is_teacher)
+            clients_list = сlients.objects.filter(is_teacher=is_teacher)
+            
+            if not clients_list.exists():
+                return Response({'error': 'База данных пуста'}, status=status.HTTP_404_NOT_FOUND)
+
         else:
-            return Response({'error': 'Обязательный параметр is_teacher не передан'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Обязательный параметр is_teacher не передан'}, 
+                            status=status.HTTP_400_BAD_REQUEST)
 
         client_data = [client.client_name for client in clients_list]
 
