579c5adfdc5f37654700bdacb920d7b3c22272c6
XaoriGit <xaoriNit@gmail.com>
2025-02-14 17:22:34 +0600

    new serialzer

--- api/schedule_api/serializers.py
+++ api/schedule_api/serializers.py
@@ -20,6 +20,7 @@ class ScheduleSerializer(serializers.ModelSerializer):
         model = Schedule
         fields = ['date', 'lessons']
 
+
 class ClientSerializer(serializers.ModelSerializer):
     schedules = ScheduleSerializer(many=True)
 
@@ -32,24 +33,35 @@ class ClientSerializer(serializers.ModelSerializer):
         
         client_name = validated_data.get('client_name')
         is_teacher = validated_data.get('is_teacher')
-        client, created = Client.objects.get_or_create(client_name=client_name, is_teacher=is_teacher)
+        client, _ = Client.objects.get_or_create(
+            client_name=client_name, 
+            is_teacher=is_teacher
+        )
 
         for schedule_data in schedules_data:
             lessons_data = schedule_data.pop('lessons', [])
-            schedule_date = schedule_data['date']
+            schedule_date = schedule_data.pop('date')
             
-            schedule, _ = Schedule.objects.get_or_create(client=client, date=schedule_date)
+            schedule, _ = Schedule.objects.update_or_create(
+                client=client,
+                date=schedule_date,
+                defaults=schedule_data
+            )
             
             Lesson.objects.filter(schedule=schedule).delete()
+            
             for lesson_data in lessons_data:
                 items_data = lesson_data.pop('items', [])
-                lesson_number = lesson_data['number']
+                lesson_number = lesson_data.get('number')
                 
-                lesson, _ = Lesson.objects.get_or_create(schedule=schedule, number=lesson_number)
+                lesson = Lesson.objects.create(
+                    schedule=schedule,
+                    **lesson_data
+                )
                 
                 ItemLesson.objects.filter(lesson=lesson).delete()
                 for item_data in items_data:
-                    item_lesson, created = ItemLesson.objects.get_or_create(lesson=lesson, **item_data)
+                    ItemLesson.objects.create(lesson=lesson, **item_data)
 
         return client
 
