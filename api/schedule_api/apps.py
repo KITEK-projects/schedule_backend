9c2f9e79cc6f8cb5c0f73bb466ea3b5d1f349906
xaoriGit <xaorinit@gmail.com>
2025-04-27 08:15:55 +0600

    fix: init_users

--- api/schedule_api/apps.py
+++ api/schedule_api/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-from django.core.management import call_command
 
-def init_users(sender, **kwargs):
-    call_command('init_users')
 
 class ScheduleApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'schedule_api'
-
-    def ready(self):
-        post_migrate.connect(init_users, sender=self)
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "schedule_api"
